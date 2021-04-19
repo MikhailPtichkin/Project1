2b7f33b1f7a6af9ba840f71a8b5525cd2126d5bf
Misha <mikeptichkin5@gmail.com>
2021-04-19 18:04:55 +0300

    первая домашняя работа, фильмы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function strict() {
-  'use strict';
-}
+ 'use strict';
+ }
 // let number = 5;
 // const leftBorderWith = 1;
 
@@ -45,40 +45,66 @@ function strict() {
 // alert(`Привет, ${user}`);
 
 //                      ОПЕРАТОРЫ
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-incr++;
-decr--;
+// incr++;
+// decr--;
 
-console.log(incr++); //постфиксный оператор увеличения на один
+//console.log(incr++); постфиксный оператор увеличения на один
 //в данном случае выводит на консоль значение и только потом  увеличивает на 1
 
-console.log(--decr);
+//console.log(--decr);
 // префиксный оператор уменьшения на 1, сначала уменьшает, потом выводит
 // результат на консоль
 
 
-const isChecked = true,   
-      isClose   = true;
+// const isChecked = true,   
+//       isClose   = true;
 
-console.log(isChecked && isClose);   // оператор И, возвращает тру, только 
+//console.log(isChecked && isClose);   // оператор И, возвращает тру, только 
 // если все переменные тру, их может быть 2 и больше
-const isChecked1 = true,
-      isClose1   = false,
-      isClose2   = false;
+// const isChecked1 = true,
+//       isClose1   = false,
+//       isClose2   = false;
 
-console.log(isChecked1 || isClose1 || isClose2);
+// console.log(isChecked1 || isClose1 || isClose2);
 //  оператор ИЛИ, который возвращает тру, если хотя бы одна из переменных тру
 
-console.log(!isChecked1 || isClose1);
+//console.log(!isChecked1 || isClose1);
 // оператор отрицания ! меняет значение переменной на противоположное
 
 // порядок выполнение операторов
-console.log(2 + 2 * 2 !== '8');
+//console.log(2 + 2 * 2 !== '8');
 // см таблицу операторов
 // = оператор присвоения
 // == оператор сравнения по типу данных, например число или строка
 // === сравнение и по типу и по значению
 // != не равен по значению
 // !== не равен по типу данных, если да, то возвращает тру 
+
+
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели", "");
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actours: {},
+genres: [],
+privat: false
+
+};
+const lastFilm = prompt("Какой последний фильм Вы смотрели?", ""), 
+      lastFilmRate = prompt("На сколько оцените его?", ""),
+      lastFilm1 = prompt("Какой последний фильм Вы смотрели?", ""), 
+      lastFilmRate1 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastFilm] = lastFilmRate;
+personalMovieDB.movies[lastFilm1] = lastFilmRate1;
+
+
+console.log(personalMovieDB);
+
+
+
+
